053b657392e87aa53b43661a6cb9b2c4cb4d6077
Anshuman Sanghvi <anshuman.sanghvi@gmail.com>
2023-06-28 10:08:30 +0530

    Refactor: remove unused projection
    Signed-off-by: Anshuman Sanghvi <anshuman.sanghvi@gmail.com>

--- src/main/java/com/anshuman/graphqldemo/model/repository/projection/BasicFilmInfoRecord.java
+++
@@ -1,8 +0,0 @@
-package com.anshuman.graphqldemo.model.repository.projection;
-
-/**
- * Projection for {@link com.anshuman.graphqldemo.model.entity.Film}
- */
-public record BasicFilmInfoRecord(Integer id, String title, String description, Integer releaseYear, Short length) {
-
-}
