dd04d3a405c431240474b6d197c3cb4909468ba9
Anshuman Sanghvi <anshuman.sanghvi@gmail.com>
2023-06-28 10:38:24 +0530

    Add request header authorization for actor query
    Signed-off-by: Anshuman Sanghvi <anshuman.sanghvi@gmail.com>

--- src/main/java/com/anshuman/graphqldemo/resource/controller/ActorGQController.java
+++ src/main/java/com/anshuman/graphqldemo/resource/controller/ActorGQController.java
@@ -2,6 +2,7 @@ package com.anshuman.graphqldemo.resource.controller;
 
 import com.anshuman.graphqldemo.resource.dto.ActorInfoRecord;
 import com.anshuman.graphqldemo.service.ActorService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,7 @@ import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Controller
@@ -17,9 +19,16 @@ public class ActorGQController {
 
     private final ActorService actorService;
 
+    private final HttpServletRequest request;
+
     @QueryMapping
     public List<ActorInfoRecord> getActorInfoByName(@Argument @PositiveOrZero Integer pageNumber,
             @Argument @PositiveOrZero Integer pageSize, @Argument @NotNull String name) {
+
+        Optional.ofNullable(request.getHeader("auth"))
+                .filter(headerVal -> headerVal.equalsIgnoreCase("1234567"))
+                .orElseThrow(() -> new IllegalAccessError("Missing valid authorization"));
+
         return actorService.getActorInfoByName(pageNumber, pageSize, name);
     }
 }
