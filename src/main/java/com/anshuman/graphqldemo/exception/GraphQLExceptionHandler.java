9f04e731e9ac4c6f78964728929c592e21816dfa
Anshuman Sanghvi <anshuman.sanghvi@gmail.com>
2023-07-05 19:24:12 +0530

    Fix exception handling
    Signed-off-by: Anshuman Sanghvi <anshuman.sanghvi@gmail.com>

--- src/main/java/com/anshuman/graphqldemo/exception/GraphQLExceptionHandler.java
+++ src/main/java/com/anshuman/graphqldemo/exception/GraphQLExceptionHandler.java
@@ -5,48 +5,32 @@ import graphql.GraphqlErrorBuilder;
 import graphql.schema.DataFetchingEnvironment;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.graphql.data.method.annotation.GraphQlExceptionHandler;
 import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter;
 import org.springframework.graphql.execution.ErrorType;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 
 import java.util.Objects;
 
 @Component
+@ControllerAdvice
 @Slf4j
 public class GraphQLExceptionHandler extends DataFetcherExceptionResolverAdapter {
 
 
+    @GraphQlExceptionHandler
     @Override
     protected GraphQLError resolveToSingleError(Throwable ex, DataFetchingEnvironment env) {
         GraphQLError graphQLError = null;
         Objects.requireNonNull(ex);
         Objects.requireNonNull(env);
-
-        if (ex instanceof AccessDeniedException) {
-            graphQLError = GraphqlErrorBuilder.newError()
-                    .errorType(ErrorType.FORBIDDEN)
-                    .message("Access denied for this path")
-                    .path(env.getExecutionStepInfo().getPath())
-                    .location(env.getField().getSourceLocation())
-                    .build();
-        } else if (ex instanceof AuthenticationException) {
-            graphQLError = GraphqlErrorBuilder.newError()
-                    .errorType(ErrorType.UNAUTHORIZED)
-                    .message("Not authorized for this path/operation")
-                    .path(env.getExecutionStepInfo().getPath())
-                    .location(env.getField().getSourceLocation())
-                    .build();
-        }
-        else {
-            graphQLError = GraphqlErrorBuilder.newError()
-                    .errorType(ErrorType.INTERNAL_ERROR)
-                    .message("Internal error encountered")
-                    .path(env.getExecutionStepInfo().getPath())
-                    .location(env.getField().getSourceLocation())
-                    .build();
-        }
+        graphQLError = GraphqlErrorBuilder.newError()
+                .errorType(ErrorType.INTERNAL_ERROR)
+                .message("Internal error encountered")
+                .path(env.getExecutionStepInfo().getPath())
+                .location(env.getField().getSourceLocation())
+                .build();
 
         Throwable rootCause = ExceptionUtils.getRootCause(Objects.requireNonNull(ex));
         log.error("GraphQLExceptionHandler: Exception encountered: {}, env: {}", rootCause.getMessage(), env, rootCause);
--- src/main/java/com/anshuman/graphqldemo/exception/RestExceptionHandler.java
+++ src/main/java/com/anshuman/graphqldemo/exception/RestExceptionHandler.java
@@ -3,8 +3,6 @@ package com.anshuman.graphqldemo.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -24,28 +22,6 @@ public class RestExceptionHandler {
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
                 new Date(),
                 "Internal error encountered.",
-                request.getDescription(false));
-    }
-
-    @ExceptionHandler(value = {AccessDeniedException.class})
-    @ResponseStatus(value = HttpStatus.FORBIDDEN)
-    public ErrorMessage accessDeniedException(AccessDeniedException ex, WebRequest request) {
-        logException(request, ex);
-        return new ErrorMessage(
-                HttpStatus.FORBIDDEN.value(),
-                new Date(),
-                "Access denied for user.",
-                request.getDescription(false));
-    }
-
-    @ExceptionHandler(value = {AuthenticationException.class})
-    @ResponseStatus(value = HttpStatus.UNAUTHORIZED)
-    public ErrorMessage authenticationException(AuthenticationException ex, WebRequest request) {
-        logException(request, ex);
-        return new ErrorMessage(
-                HttpStatus.UNAUTHORIZED.value(),
-                new Date(),
-                "User not authenticated.",
                 request.getDescription(false));
     }
 
