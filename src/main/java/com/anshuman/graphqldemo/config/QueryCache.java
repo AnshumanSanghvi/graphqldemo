c2bdde108ea7bd79854f752f3569bbf2fe0d9d3b
Anshuman Sanghvi <anshuman.sanghvi@gmail.com>
2024-05-31 18:13:50 +0530

    Add cache bean for graphql
    Signed-off-by: Anshuman Sanghvi <anshuman.sanghvi@gmail.com>

---
+++ src/main/java/com/anshuman/graphqldemo/config/QueryCache.java
@@ -0,0 +1,17 @@
+package com.anshuman.graphqldemo.config;
+
+import graphql.execution.preparsed.persisted.InMemoryPersistedQueryCache;
+import graphql.execution.preparsed.persisted.PersistedQueryCache;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+
+@Configuration
+public class QueryCache {
+
+    @Bean
+    public PersistedQueryCache persistedQueryCache() {
+        return new InMemoryPersistedQueryCache(new HashMap<Object, String>(100));
+    }
+}
