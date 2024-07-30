76e9b5305546df21fe6837708bbe2cbe97702842
Anshuman Sanghvi <anshuman.sanghvi@gmail.com>
2024-07-31 03:12:14 +0530

    Enable Spring Async, Ensure that the same executor is used throughout the application concurrency.
    Signed-off-by: Anshuman Sanghvi <anshuman.sanghvi@gmail.com>

--- src/main/java/com/anshuman/graphqldemo/config/ConcurrencyConfig.java
+++ src/main/java/com/anshuman/graphqldemo/config/ConcurrencyConfig.java
@@ -5,11 +5,19 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.graphql.data.method.annotation.support.AnnotatedControllerConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 @Configuration
-public class ControllerConcurrencyConfig {
+@EnableAsync
+public class ConcurrencyConfig {
+
+    @Bean(name = "APIThreadExecutor")
+    public Executor getExecutor() {
+        return Executors.newVirtualThreadPerTaskExecutor();
+    }
 
     @Bean
     public AnnotatedControllerConfigurer annotatedControllerConfigurer(@Qualifier("APIThreadExecutor") Executor executor) {
