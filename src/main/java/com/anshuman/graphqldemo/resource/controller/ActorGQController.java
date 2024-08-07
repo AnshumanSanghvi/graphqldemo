8fdf8f63803b883e6fd1407d4f3bd81225788b1e
Anshuman Sanghvi <anshuman.sanghvi@gmail.com>
2024-08-07 10:12:40 +0530

    Refactor: Move graphql resolvers to appropriate controllers
    Signed-off-by: Anshuman Sanghvi <anshuman.sanghvi@gmail.com>

--- src/main/java/com/anshuman/graphqldemo/resource/controller/ActorGQController.java
+++ src/main/java/com/anshuman/graphqldemo/resource/controller/ActorGQController.java
@@ -1,24 +1,37 @@
 package com.anshuman.graphqldemo.resource.controller;
 
-import com.anshuman.graphqldemo.resource.dto.ActorInfoRecord;
+import com.anshuman.graphqldemo.model.entity.Actor;
+import com.anshuman.graphqldemo.model.entity.Film;
+import com.anshuman.graphqldemo.model.entity.FilmActor;
+import com.anshuman.graphqldemo.model.entity.FilmActorId;
+import com.anshuman.graphqldemo.resource.dto.*;
 import com.anshuman.graphqldemo.service.ActorService;
+import com.anshuman.graphqldemo.service.FilmActorService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.BatchMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toMap;
 
 @RequiredArgsConstructor
 @Controller
 public class ActorGQController {
 
     private final ActorService actorService;
-
+    private final FilmActorService filmActorService;
     private final HttpServletRequest request;
 
     @QueryMapping
@@ -29,5 +42,48 @@ public class ActorGQController {
                 .filter(headerVal -> headerVal.equalsIgnoreCase("1234567"))
                 .map(x -> actorService.getActorInfoByName(pageNumber, pageSize, name))
                 .orElseThrow(() -> new IllegalAccessError("Missing valid authorization"));
+    }
+
+    @SchemaMapping(typeName = "Film", value = "filmActors")
+    public Set<FilmActorRecord> filmActors(FilmRecord filmRecord) {
+        return filmActorService.getFilmActorByFilmId(filmRecord.id());
+    }
+
+    @SchemaMapping(typeName = "FilmActor", value = "id")
+    public FilmActorIdRecord filmActorId(FilmActorRecord filmActor) {
+        return filmActor.id();
+    }
+
+    @SchemaMapping(typeName = "FilmActor", value = "actor")
+    public ActorRecord actor(FilmActorRecord filmActor) {
+        return actorService.getByActorId(filmActor.id().actorId());
+    }
+
+    @BatchMapping(typeName = "Film2", value = "filmActors")
+    public Mono<Map<Film, Set<FilmActor>>> filmActors(Set<Film> films) {
+        Set<Integer> filmIds = films.stream().map(Film::getId).collect(Collectors.toSet());
+        return Mono.fromFuture(filmActorService.getFilmActorsByFilmIds(filmIds))
+                .map(filmActors -> films.stream()
+                        .collect(toMap(film -> film,
+                                film -> filmActors.stream()
+                                        .filter(filmActor -> Integer.valueOf(filmActor.getFilmId().intValue()).equals(film.getId()))
+                                        .collect(Collectors.toSet()))));
+    }
+
+    @BatchMapping(typeName = "FilmActor2", value = "id")
+    public Mono<Map<FilmActor, FilmActorId>> filmActorId(Set<FilmActor> filmActors) {
+        return Mono.just(filmActors.stream().collect(toMap(fac -> fac, FilmActor::getId)));
+    }
+
+    @BatchMapping(typeName = "FilmActor2", value = "actor")
+    public Mono<Map<FilmActor, Actor>> actor(Set<FilmActor> filmActors) {
+        var actorIds = filmActors.stream().map(FilmActor::getId).map(FilmActorId::getActorId).collect(Collectors.toSet());
+        return Mono.fromFuture(actorService.getActorsByActorId(actorIds))
+                .map(actors -> filmActors.stream()
+                        .collect(toMap(filmActor -> filmActor,
+                                fa -> actors.stream()
+                                        .filter(actor -> actor.getId().equals(fa.getId().getActorId().intValue()))
+                                        .findFirst()
+                                        .orElse(new Actor()))));
     }
 }
--- src/main/java/com/anshuman/graphqldemo/resource/controller/FilmGQController.java
+++ src/main/java/com/anshuman/graphqldemo/resource/controller/FilmGQController.java
@@ -1,11 +1,11 @@
 package com.anshuman.graphqldemo.resource.controller;
 
-import com.anshuman.graphqldemo.resource.dto.*;
-import com.anshuman.graphqldemo.service.*;
+import com.anshuman.graphqldemo.model.entity.Film;
+import com.anshuman.graphqldemo.resource.dto.FilmRecord;
+import com.anshuman.graphqldemo.service.FilmService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
@@ -16,11 +16,6 @@ import java.util.Set;
 public class FilmGQController {
 
     private final FilmService filmService;
-    private final LanguageService languageService;
-    private final FilmCategoryService filmCategoryService;
-    private final CategoryService categoryService;
-    private final FilmActorService filmActorService;
-    private final ActorService actorService;
 
     @QueryMapping
     public List<FilmRecord> getFilmsDetailedByTitle(@Argument String title) {
@@ -32,39 +27,10 @@ public class FilmGQController {
         return filmService.getFilmRecordsByTitle(title);
     }
 
-    @SchemaMapping(typeName = "Film", value = "language")
-    public LanguageRecord language(FilmRecord filmRecord) {
-        return languageService.getLanguageRecordById(filmRecord.language());
+    @QueryMapping
+    public Set<Film> getFilms2ByTitle(@Argument String title) {
+        return filmService.getFilmsByTitle(title);
     }
 
-    @SchemaMapping(typeName = "Film", value = "filmCategories")
-    public Set<FilmCategoryRecord> filmCategories(FilmRecord filmRecord) {
-        return filmCategoryService.getFilmCategoryByFilmId(filmRecord.id());
-    }
-
-    @SchemaMapping(typeName = "FilmCategory", value = "id")
-    public FilmCategoryIdRecord filmCategoryId(FilmCategoryRecord filmCategory) {
-        return filmCategory.id();
-    }
-
-    @SchemaMapping(typeName = "FilmCategory", value = "category")
-    public CategoryRecord category(FilmCategoryRecord filmCategory) {
-        return categoryService.getCategoryRecordById(Integer.valueOf(filmCategory.id().categoryId()));
-    }
-
-    @SchemaMapping(typeName = "Film", value = "filmActors")
-    public Set<FilmActorRecord> filmActors(FilmRecord filmRecord) {
-        return filmActorService.getFilmActorByFilmId(filmRecord.id());
-    }
-
-    @SchemaMapping(typeName = "FilmActor", value = "id")
-    public FilmActorIdRecord filmActorId(FilmActorRecord filmActor) {
-        return filmActor.id();
-    }
-
-    @SchemaMapping(typeName = "FilmActor", value = "actor")
-    public ActorRecord actor(FilmActorRecord filmActor) {
-        return actorService.getByActorId(filmActor.id().actorId());
-    }
 
 }
